babf7c74be50610c13e9e407499ada168fa4f6e3
JiSeungHun <vhzpt0108@gmail.com>
2026-05-24 21:08:30 +0900

    Update 2-7.c

--- code/2-7.c
+++ code/2-7.c
@@ -17,9 +17,9 @@ void config_parser(Config* config_ptr){
         return;
 
     fscanf(fp, "InputFileName=%s", config_ptr->InputFileName);
-    fscanf(fp, "Options=%d", &config_ptr->Options);
-    fscanf(fp, "SectionName=%s", config_ptr->SectionName);
-    fscanf(fp, "Address=%llx", &config_ptr->Address);
+    fscanf(fp, " Options=%d", &config_ptr->Options);      
+    fscanf(fp, " SectionName=%s", config_ptr->SectionName); 
+    fscanf(fp, " Address=%llx", &config_ptr->Address);     
 
     fclose(fp);
 }
