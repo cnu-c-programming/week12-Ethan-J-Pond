b3702706c5c35ddbb4135516f4b81932452c407f
JiSeungHun <vhzpt0108@gmail.com>
2026-05-24 17:40:31 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -1,28 +1,39 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
 
-typedef struct student {
+typedef struct student{
     char name[64];
     int score;
 } Student;
 
-int main(int argc, const char* argv[]) {
-    FILE* fp = fopen("student.txt", "r");
+int main(int argc, const char* argv[]){
+    FILE* fp = fopen("student.txt","w");
     int count = 0;
     Student students[64];
-
-
-
+    
+    while(!feof(fp)){
+        if(fscanf(fp,"%s %d",students[count].name,&stduents[count].score) == 2)
+            counts++;
+    }
     int max = 0;
     float avg = 0;
 
+    
+    int sum = 0;
+    for (int i = 0; i < count; i++) {
+        if (students[i].score > max)
+            max = students[i].score;
+
+        sum += students[i].score;
+    }
+
+    if (count > 0)
+        avg = (float)sum / count;
 
 
-    printf("max: %d\n", max);
-    printf("avg: %.2f\n", avg);
-
+    printf("max: %d\n",max);
+    printf("avg: %.2f\n",avg);
+    
     fclose(fp);
-
     return 0;
 }
-
