81d756ef61c14b18c50b450580c9f9a83ab5ef41
JiSeungHun <vhzpt0108@gmail.com>
2026-05-24 18:03:21 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -1,13 +1,22 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(int argc, const char* argv[]) {
-    if (argc < 2)
+
+int main(int argc, const char* argv[]){
+    if(argc < 2)
         return 0;
-
     FILE* fp = fopen(argv[1], "r");
 
+    int alphaFreq[26] = {0};
+    int c;
 
+    while((c = fgetc(fp)) != EOF){
+        if(c >= 'a' && c <= 'z')
+            alphaFreq[c - 'a']++;
+    }
+    
+    for(int i = 0; i < 26 ; i++)
+        printf("%c: %d\n", 'a' + i, alphaFreq[i]);
 
     fclose(fp);
+    return 0;
 }
-
